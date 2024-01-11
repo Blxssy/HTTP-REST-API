62a16d512e888a9746fdb5c0bf96255cfb706d6b
Blxssy <roma.asav@gmail.com>
2024-01-11 16:28:56 +0300

    Made validation tests

--- internal/app/model/user.go
+++ internal/app/model/user.go
@@ -1,7 +1,48 @@
 package model
 
+import (
+	validation "github.com/go-ozzo/ozzo-validation"
+	"github.com/go-ozzo/ozzo-validation/is"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// User
 type User struct {
 	ID                 int
 	Email              string
+	Password           string
 	EncryptedPasssword string
+}
+
+// BeforeCreate
+func (u *User) BeforeCreate() error {
+	if len(u.Password) > 0 {
+		enc, err := encryptString(u.Password)
+		if err != nil {
+			return err
+		}
+
+		u.EncryptedPasssword = enc
+	}
+
+	return nil
+}
+
+// Validate
+func (u *User) Validate() error {
+	return validation.ValidateStruct(
+		u,
+		validation.Field(&u.Email, validation.Required, is.Email),
+		validation.Field(&u.Password, validation.By(requiredIF(u.EncryptedPasssword == "")), validation.Length(6, 100)),
+	)
+}
+
+// encryptString
+func encryptString(s string) (string, error) {
+	b, err := bcrypt.GenerateFromPassword([]byte(s), bcrypt.MinCost)
+	if err != nil {
+		return "", err
+	}
+
+	return string(b), nil
 }
